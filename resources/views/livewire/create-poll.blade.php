b3e7ed8ed05b1be2ae0adc0f417cab75a834b3dd
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-07-20 19:16:54 +0200

    feat: add actions

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,9 +1,20 @@
 <div>
     <form>
-      <label>Poll Title</label>
-  
-      <input type="text" wire:model="title">
-  
-      Current title: {{ $title }}
+        <label>Poll Title</label>
+    
+        <input type="text" wire:model="title">
+    
+        Current title: {{ $title }}
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+            
+        </div>
+        <div>
+            @foreach ($options as $index => $option)
+                <div class="mb-4">
+                    {{ $index}} - {{ $option }}
+                </div>
+            @endforeach
+        </div>
     </form>
 </div>
