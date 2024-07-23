802507c32fc9214a032eba0f80787e31310813f1
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-07-23 20:27:27 +0200

    feat: validation in Livewire

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -3,6 +3,10 @@
         <label>Poll Title</label>
     
         <input type="text" wire:model="title">
+
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
     
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
@@ -12,9 +16,12 @@
                 <div class="mb-4">
                    <label>Option {{ $index + 1 }}</label>
                     <div class="flex gap-2">
-                    <input type="text" wire:model="options.{{ $index }}">
+                    <input type="text" wire:model="options.{{ $index }}" >
                     <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             @endforeach
         </div>
